ee0610873f171d1d45a0c8da1fca867a51e8dc89
Muhammad Ali Raza <155912766+Raza0043@users.noreply.github.com>
2024-10-11 00:30:27 +0500

    Add files via upload

---
+++ Assign # 11/app.js
@@ -0,0 +1,97 @@
+// Question no # 01
+
+// var input = prompt ("Enter a positive no.")
+// // a
+// document.write("input number : "+ input+"<br>")
+// //  b
+// var round_no = Math.round(input)
+// document.write("Round no: " +round_no+"<br>")
+// // c
+// var floor_1 = Math.floor(input);
+// document.write("Floor no. is " + floor_1+"<br>")
+// // d
+// var ceil_no = Math.ceil(input);
+// document.write("Ceil no : " + ceil_no )
+
+// Question no # 02
+
+// var input =parseFloat( prompt ("Enter a negative no."))
+// // a
+// document.write("input number : "+ input+"<br>")
+// //  b
+// var round_no = Math.round(input)
+// document.write("Round no: " +round_no+"<br>")
+// // c
+// var floor_1 = Math.floor(input);
+// document.write("Floor no. is " + floor_1+"<br>")
+// // d
+// var ceil_no = Math.ceil(input);
+// document.write("Ceil no : " + ceil_no )
+
+// Question no # 03
+
+//  var input = prompt ("Enter no.");
+// var abs_no = Math.abs(Number(input));
+//  document.write("The absolut value of  : "  +input+ +"is"+ abs_no+"<br>");
+
+// Question no # 04
+
+
+// var rand_no =   Math.random()*4;
+// var ceil_number = Math.ceil(rand_no);
+// document.write("Random dice value  : "  + ceil_number   +   "<br>");
+
+
+// // Question no # 05
+// var rand_no =   Math.random()*2;
+// var ceil_number = Math.ceil(rand_no);
+//  document.write("Random dice value  : "  + ceil_number   +   "<br>");
+// if (ceil_number==1)
+// {
+    
+//     document.write(ceil_number+"<br>"+"Random  coin value is " +"<br>"+"The option is Heads")
+// }
+//     else if(ceil_number==2)
+
+// {
+// document.write(ceil_number+"<br>"+"Random  coin value is : "+"<br>"+"The option is Tales" )
+
+// }
+
+
+ // Question no # 06
+
+//  var rand_no =   Math.random()*100;
+
+// var ceil_number = Math.ceil(rand_no);
+//  document.write("Random  number between 0 to 100  : "  + ceil_number   +   "<br>");
+
+ // Question no # 07
+ 
+ 
+
+//  var userInput = prompt("Please enter your weight:");
+
+//  var parsedWeight = parseFloat(userInput);
+
+//  if (!isNaN(parsedWeight)) {
+//      document.write("Your weight is: " + parsedWeight + " kilograms");
+//  } else {
+//      document.write("Invalid input. Please enter a valid weight.");
+//  }
+
+  // Question no # 08
+
+
+//    var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+   
+//    var userGuess = parseInt(prompt("Guess a number between 1 and 10:"));
+
+//    if (userGuess === secretNumber) {
+//        document.write("Congratulations! You guessed the correct number: " + secretNumber);
+//    } else {
+//        document.write("Sorry, wrong guess! The correct number was: " + secretNumber);
+//    }
+
+
